01e5832f14cdd8c074c703e83fd2554ee9f0351d
Yomna Osama <yomna.osamma@gmail.com>
2023-05-12 19:58:41 +0300

    Adding some styles in home page

--- frontend-react/find-it/src/App.js
+++ frontend-react/find-it/src/App.js
@@ -7,14 +7,16 @@ import search from './search-icon.svg';
 function App() {
   const [query, setQuery] = useState("");
   return (
-    <div className='parent-Div'>
-      <div className='logo-div'>
-        <img src={logo} alt="text" height={60}/>
-        <p className='header'>Find-it</p>
-      </div>
-      <div className='search-div'>
-        <img src={search} alt="text"/>
-        <input type='search' placeholder='Search Find-it' className='search-bar' onChange={e=> setQuery(e.target.value)} />
+    <div className='homepage'>
+      <div className='parent-Div'>
+        <div className='logo-div'>
+          <img src={logo} alt="text" height={60}/>
+          <p className='header'>Find-it</p>
+        </div>
+        <div className='search-div'>
+          <img src={search} alt="text"/>
+          <input type='search' placeholder='Search Find-it' className='search-bar' onChange={e=> setQuery(e.target.value)} />
+        </div>
       </div>
     </div>
   );
