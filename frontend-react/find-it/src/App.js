ad847ecf5d6b3ce36b6707d61b3801cc05ad885c
Yomna Osama <yomna.osamma@gmail.com>
2023-05-12 19:43:40 +0300

    Putting first react page

--- frontend-react/find-it/src/App.js
+++ frontend-react/find-it/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import logo from './logo.jpg';
+import search from './search-icon.svg';
+
 
 function App() {
+  const [query, setQuery] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='parent-Div'>
+      <div className='logo-div'>
+        <img src={logo} alt="text" height={60}/>
+        <p className='header'>Find-it</p>
+      </div>
+      <div className='search-div'>
+        <img src={search} alt="text"/>
+        <input type='search' placeholder='Search Find-it' className='search-bar' onChange={e=> setQuery(e.target.value)} />
+      </div>
     </div>
   );
 }
-
 export default App;
